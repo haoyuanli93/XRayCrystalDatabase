524a5098f5711c96a22dd9af5e128ad0591a5924
Haoyuan Li <hyli16@stanford.edu>
2020-09-25 10:12:16 -0700

    update Ge crystal

--- XRayCrystalDatabase/Database/xserver_benchmark_result.py
+++ XRayCrystalDatabase/Database/xserver_benchmark_result.py
@@ -130,4 +130,66 @@ diamond_11kev = {"1,1,1": {"chi0": complex(-0.12067E-04, 0.82462E-08),
 #################################################################
 #               Germanium
 #################################################################
-# TODO: Get the data for Germanium
+germanium_8kev = {"1,1,1": {"chi0": complex(-0.29188E-04, 0.83651E-06),
+                            "chih sigma": complex(-0.17348E-04, 0.58374E-06),
+                            "chih pi": complex(-0.15396E-04, 0.51539E-06)},
+
+                  "2,2,0": {"chi0": complex(-0.29188E-04, 0.83651E-06),
+                            "chih sigma": complex(-0.20796E-04, 0.80755E-06),
+                            "chih pi": complex(-0.14555E-04, 0.55703E-06)},
+
+                  "3,3,3": {"chi0": complex(-0.29188E-04, 0.83651E-06),
+                            "chih sigma": complex(-0.97802E-05, 0.52540E-06),
+                            "chih pi": complex(-0.12666E-06, 0.18260E-07)},
+
+                  "4,4,0": {"chi0": complex(-0.29188E-04, 0.83651E-06),
+                            "chih sigma": complex(-0.12645E-04, 0.73275E-06),
+                            "chih pi": complex(-0.25358E-05, 0.16094E-06)}}
+
+germanium_9kev = {"1,1,1": {"chi0": complex(-0.22831E-04, 0.52915E-06),
+                            "chih sigma": complex(-0.13545E-04, 0.36935E-06),
+                            "chih pi": complex(-0.12341E-04, 0.33498E-06)},
+
+                  "2,2,0": {"chi0": complex(-0.22831E-04, 0.52915E-06),
+                            "chih sigma": complex(-0.16207E-04, 0.51117E-06),
+                            "chih pi": complex(-0.12363E-04, 0.38475E-06)},
+
+                  "3,3,3": {"chi0": complex(-0.22831E-04, 0.52915E-06),
+                            "chih sigma": complex(-0.75790E-05, 0.33292E-06),
+                            "chih pi": complex(-0.15131E-05, 0.73703E-07)},
+
+                  "4,4,0": {"chi0": complex(-0.22831E-04, 0.52915E-06),
+                            "chih sigma": complex(-0.97850E-05, 0.46073E-06),
+                            "chih pi": complex(-0.50324E-06, 0.35022E-07)}}
+
+germanium_10kev = {"1,1,1": {"chi0": complex(-0.18164E-04, 0.35107E-06),
+                             "chih sigma": complex(-0.10742E-04, 0.24510E-06),
+                             "chih pi": complex(-0.99678E-05, 0.22652E-06)},
+
+                   "2,2,0": {"chi0": complex(-0.18164E-04, 0.35107E-06),
+                             "chih sigma": complex(-0.12808E-04, 0.33933E-06),
+                             "chih pi": complex(-0.10348E-04, 0.27095E-06)},
+
+                   "3,3,3": {"chi0": complex(-0.18164E-04, 0.35107E-06),
+                             "chih sigma": complex(-0.59279E-05, 0.22130E-06),
+                             "chih pi": complex(-0.20849E-05, 0.81661E-07)},
+
+                   "4,4,0": {"chi0": complex(-0.18164E-04, 0.35107E-06),
+                             "chih sigma": complex(-0.76327E-05, 0.30637E-06),
+                             "chih pi": complex(-0.17682E-05, 0.78064E-07)}}
+
+germanium_11kev = {"1,1,1": {"chi0": complex(-0.13836E-04, 0.24210E-06),
+                             "chih sigma": complex(-0.80552E-05, 0.16905E-06),
+                             "chih pi": complex(-0.75757E-05, 0.15841E-06)},
+
+                   "2,2,0": {"chi0": complex(-0.13836E-04, 0.24210E-06),
+                             "chih sigma": complex(-0.94430E-05, 0.23412E-06),
+                             "chih pi": complex(-0.79440E-05, 0.19490E-06)},
+
+                   "3,3,3": {"chi0": complex(-0.13836E-04, 0.24210E-06),
+                             "chih sigma": complex(-0.41450E-05, 0.15286E-06),
+                             "chih pi": complex(-0.19242E-05, 0.72595E-07)},
+
+                   "4,4,0": {"chi0": complex(-0.13836E-04, 0.24210E-06),
+                             "chih sigma": complex(-0.52606E-05, 0.21168E-06),
+                             "chih pi": complex(-0.19202E-05, 0.81120E-07)}}
